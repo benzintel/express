cedc15dc27f9ce68a9cf097650e95b366a5c2fdd
sarut yentakham <sarut.y@bitkub.com>
2018-06-23 17:50:51 +0700

    Add function Render html

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
-const app = express()
 const express = require('express')
 const dotenv  = require('dotenv')
-const port = 3000;
+const app = express()
 
-app.get('/', (req, res) => 
+const beseProject = __dirname
+const basePathHTML = beseProject + '/views/'
+const basePathPublic = beseProject + '/public'
+
+
+//.env
+dotenv.config()
+const port = process.env.START_PORT;
+
+// GET
+app.get('/', (req, res) => {
 	res.send('Hello World!')
-)
+})
 
+// RENDER HTML
+app.get('/test', (req, res) => {
+	res.sendFile(basePathHTML + 'index.html');
+})
+
+// POST
 app.post('/', function (req, res) {
   res.send('Got a POST request')
 })
 
+// PUT
 app.put('/user', function (req, res) {
   res.send('Got a PUT request at /user')
 })
 
+// DELETE
 app.delete('/user', function (req, res) {
   res.send('Got a DELETE request at /user')
 })
 
-// Static Files
-app.use('/static', express.static(__dirname + '/public'))
+// SET Static folder
+app.use('/static', express.static(basePathPublic))
 
+// Start Server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
